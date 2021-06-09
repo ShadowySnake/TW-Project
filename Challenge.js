9256100b9dbc32746c0be5f8184425e1992f15bc
Adrian Knight <Adrianknight38@gmail.com>
2021-06-09 16:31:14 +0300

    Small game update.
    Corrected one of our mistakes, made the  Challenge accept/ have an ok-ish preview.

--- Challenge.js
+++ Challenge.js
@@ -1,14 +1,28 @@
-const first = document.querySelector('.first');
-const iframe = document.querySelector('iframe');
+const first = document.getElementById('first');
+const iframe = document.getElementById('second');
+const checker = document.getElementById('checker');
+const returner = document.getElementById('returner');
 
 first.addEventListener('keyup', function(){
     var html = first.textContent
     iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
 })
 
-first.addEventListener('paste', function(e){
-    e.preventDefault()
+//first.addEventListener('paste', function(e){
+//    e.preventDefault()
+//
+//    var text = e.clipboardData.getData("text/plain")
+//    document.execCommand("insertText", false, text)
+//})
 
-    var text = e.clipboardData.getData("text/plain")
-    document.execCommand("insertText", false, text)
+checker.addEventListener('click', function(){
+    // some code for cheking answers
+    alert(first.textContent);
+    // just a test for now
+})
+
+returner.addEventListener('click', function(){
+    // some code to save progress
+    window.location.replace("./WelcomingPage.php");
+    // just a test for now
 })
