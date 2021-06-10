cafc7c5f876744c2419628fc2cfec741a624cfea
Adrian Knight <Adrianknight38@gmail.com>
2021-06-10 18:04:39 +0300

    Challenge script update.
    The questtion is now shown on page.

--- Challenge.js
+++ Challenge.js
@@ -3,17 +3,44 @@ const iframe = document.getElementById('second');
 const checker = document.getElementById('checker');
 const returner = document.getElementById('returner');
 
+questionGetter();
+
+async function questionGetter(){
+    let item = document.createElement("div");
+    if(sessionStorage.getItem('questionsAnswered') == null) item.innerText = "Press the 'Click Me' button!";
+    else {
+            let URL = "http://localhost/TW-Project/api/answers/read.php";
+            let number = parseInt(sessionStorage.getItem('questionsAnswered'));
+            console.log(number);
+            fetch(URL, {
+                method : 'GET',
+                headers : new Headers({
+                    "Accept": "application/json"
+                }),
+            }).then(resp => {
+                console.log("Answer description arrived!")
+                return resp.json();
+            }).then(jsonResp => {
+                console.log(jsonResp);
+                if(number >= jsonResp.length) item.innerText = "Finished!"
+                else item.innerText = jsonResp[number].description;
+            })
+    }
+
+    document.body.appendChild(item);
+}
+
 first.addEventListener('keyup', function(){
     var html = first.textContent
     iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
 })
 
-//first.addEventListener('paste', function(e){
-//    e.preventDefault()
-//
-//    var text = e.clipboardData.getData("text/plain")
-//    document.execCommand("insertText", false, text)
-//})
+first.addEventListener('paste', function(e){
+   e.preventDefault();
+   var text = e.clipboardData.getData("text/plain");
+   document.execCommand("insertText", false, text);
+})
+
 
 checker.addEventListener('click', function(){
     // some code for cheking answers
@@ -66,8 +93,10 @@ checker.addEventListener('click', function(){
                 }).then(jsonResp3 => {
                     if(jsonResp3[0].answer == first.textContent) {
                         sessionStorage.setItem('questionsAnswered', number);
-                        location.reload();
-                    } else alert("Try again");
+                    } else {
+                        sessionStorage.setItem('questionsAnswered', number-1);
+                        alert("Try again");
+                    }
                 })}
             }
         })
@@ -94,9 +123,9 @@ checker.addEventListener('click', function(){
                 sessionStorage.setItem('questionsAnswered', 0);
             } else {
                 let number = parseInt(gitJsonResp[0].answernumber) + 1;
-                if(number >= 2) alert("You have finished, congratz");
+                if(number >= gitJsonResp.length) alert("You have finished, congratz");
                 else{
-                let URL3 = "http://localhost/TW-Project/api/answers/read.php?id=" + number;
+                let URL3 = "http://localhost/TW-Project/api/answers/read.php";
                 fetch(URL3, {
                     method : 'GET',
                     headers : new Headers({
@@ -105,19 +134,22 @@ checker.addEventListener('click', function(){
                 }).then(resp3 => {
                     return resp3.json();
                 }).then(jsonResp3 => {
-                    if(jsonResp3[0].answer == first.textContent) {
+                    if(jsonResp3[number].answer == first.textContent) {
                         sessionStorage.setItem('questionsAnswered', number);
-                        location.reload();
-                    } else alert("Try again");
+                    } else {
+                        sessionStorage.setItem('questionsAnswered', number-1);
+                        alert("Try again");
+                    }
                 })}
             }
         })
     }
-    // just a test for now
+    window.location.reload();
 })
 
 returner.addEventListener('click', function(){
     // some code to save progress
+    if(sessionStorage.getItem('questionsAnswered') != 0 && sessionStorage.getItem('questionsAnswered') != null){
     let array = {
         'name' : sessionStorage.getItem('name'),
         'answernumber' : sessionStorage.getItem('questionsAnswered')
@@ -127,9 +159,8 @@ returner.addEventListener('click', function(){
     xhr.open('PUT', './api/progress/update.php');
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(array));
-
+    }
     sessionStorage.clear();
     
     window.location.replace("./WelcomingPage.php");
-    // just a test for now
 })
