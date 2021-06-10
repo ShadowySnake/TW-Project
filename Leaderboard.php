e0d68ff9b38316eb913a4c8e42060a6bf94e96e3
Tighineanu Andrei <56965726+Ondrii11@users.noreply.github.com>
2021-06-11 00:19:59 +0300

    Add files via upload

--- Leaderboard.php
+++ Leaderboard.php
@@ -5,13 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leaderboard</title>
+    <link rel="stylesheet" href="./css/Leaderboard.css">
 </head>
 <body>
-    <table id="table">
+    <table class="tbl" id="table">
+        <thead>
     <tr>
         <td>Name</td>
         <td>Ellapsed Time</td>
     </tr>
+        </thead>
     <?php
       echo '<script src="./utils/js/boardGet.js"></script>'
     ?>
